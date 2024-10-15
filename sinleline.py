2f4de341a3d8f48b5100dbdf60a2149c0feb612b
divya94497 <divyagowdav65@gmail.com>
2024-10-15 10:29:04 +0530

    Create sinleline.py

---
+++ sinleline.py
@@ -0,0 +1,7 @@
+'''
+ Write a program to get input in a single line separated by space and print the values of set in single line separated by space.
+Sample Input:
+3 1 5 4 2
+'''
+a={1,2,5,4,3}
+print(a)
